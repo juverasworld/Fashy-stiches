4f1577771c3660a88099fd330cff779809d55380
juverasworld <108337782+juverasworld@users.noreply.github.com>
2023-04-22 11:06:46 -0600

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { BsGlobe } from 'react-icons/bs';
+import Head from 'next/head';
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+    useEffect(() => {
+
+        function ife() {
+            const script = document.createElement("script");
+            script.src = 'window.gtranslateSettings = {"default_language":"en","languages":["en","fr","de","it","es"],"globe_color":"#66aaff","wrapper_selector":".gtranslate_wrapper","globe_size":20}'
+
+            script.src = "https://cdn.gtranslate.net/widgets/latest/globe.js";
+            script.async = true;
+            document.body.appendChild(script);
+        }
+        ife();
+    }, []);
+    return (
+        <>
+
+            <header>
+                <nav className="nav bg-yellow-600">
+                    <div className="nav__hamburger  " onClick={toggleMenu}>
+                        <span />
+                        <span />
+                        <span />
+                    </div>
+                    {/* <div className="nav__logo  flex justify-end">
+                        <p className='px-10'>Custom Clothing</p>
+                        <p>Look Books</p>
+                        <p>Coustom Clothing</p>
+                        <p className='font-bold font-["Clash Display] text-yellow-600 text-[20px]'>Fashy Stiches</p>
+                        <p>     <div className=" border-2 w-16 h-10 globe_size wrapper_selector gtranslate_wrapper"><BsGlobe/></div></p>
+                    </div> */}
+                    <div className={`nav__menu ${menuOpen ? 'nav__menu--open' : ''}`}>
+                        <ul>
+                            <li>
+                                <a href="/">Home</a>
+                            </li>
+                            <li>
+                                <a href="/about">About Us</a>
+                            </li>
+                            <li>
+                                <a href="/contact">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </nav>
+            </header>
+        </>
+    );
+};
+
+export default Header;
---
+++ src/components/ROws.tsx
@@ -0,0 +1,122 @@
+import Image from "next/image"
+export default function Row() {
+    return (
+        <>
+        <div className="glass flex justify-evenly">
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imageA.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imageB.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imageF.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+        </div>
+         <div className="glass flex justify-evenly">
+         <div className="w-[30%] h-[90vh]">
+             <div className="image-container ">
+                 <Image className="img h-[90vh] px-2 py-10" src="/img/imageH.jpg" alt="your image" width="350" height="50" />
+             </div>
+             <p>Male Dope</p>
+             <p>#50k</p>
+         </div>
+         <div className="w-[30%] h-[90vh]">
+             <div className="image-container ">
+                 <Image className="img h-[90vh] px-2 py-10" src="/img/imageI.jpg" alt="your image" width="350" height="50" />
+             </div>
+             <p>Male Dope</p>
+             <p>#50k</p>
+         </div>
+         <div className="w-[30%] h-[90vh]">
+             <div className="image-container ">
+                 <Image className="img h-[90vh] px-2 py-10" src="/img/imageK.jpg" alt="your image" width="350" height="50" />
+             </div>
+             <p>Male Dope</p>
+             <p>#50k</p>
+         </div>
+     </div>
+     <div className="glass flex justify-evenly">
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imageL.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imagem.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imageN.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+        </div>
+        <div className="glass flex justify-evenly">
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imageO.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imageP.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imageQ.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+        </div>
+        <div className="glass flex justify-evenly">
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imageA.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imageB.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+            <div className="w-[30%] h-[90vh]">
+                <div className="image-container ">
+                    <Image className="img h-[90vh] px-2 py-10" src="/img/imageF.jpg" alt="your image" width="350" height="50" />
+                </div>
+                <p>Male Dope</p>
+                <p>#50k</p>
+            </div>
+        </div>
+     </>
+    )
+}
